e61a245d5e5d638126c65aa9aa31e2f1dbf2afa2
unknown <pawut.j@gmail.com>
2020-03-25 17:52:15 +0900

    Add coming soon in location

--- src/views/qrstat/qrstatcomp_3_table.js
+++ src/views/qrstat/qrstatcomp_3_table.js
@@ -1,44 +1,46 @@
-import React,{Component} from "react"
-import { Table } from 'reactstrap';
-import '../../app/app.css';
+import React, { Component } from "react";
+import { Table } from "reactstrap";
+import "../../app/app.css";
 
 class QRstatComp_table_3 extends Component {
-
-  render(){
-    console.log(this.props.data)
+  render() {
+    console.log(this.props.data);
     let data = Array.from(this.props.data);
-      return (
-
+    return (
       <div>
-        {/* <div style = {{color:"#F29258" , textAlign:"center" , marginTop:"3%", marginBottom:"3%"}}>
-        <h2>Coming soon</h2>
-        </div> */}
+        <div
+          style={{
+            color: "#F29258",
+            textAlign: "center",
+            marginTop: "3%",
+            marginBottom: "3%"
+          }}
+        >
+          <h2>Coming soon</h2>
+        </div>
         <Table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Time</th>
-            <th>State</th>
-            <th>SubState</th>
-
-          </tr>
-        </thead>
-        <tbody>
-        {data.map((d) =>
-          <tr>
-            <td>{d.date}</td>
-            <td>{d.time}</td>
-            <td>{d.state}</td>
-            <td>{d.suburb}</td>
-          </tr>
-        )
-        }
-        </tbody>
-      </Table>
-
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Time</th>
+              <th>State</th>
+              <th>SubState</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(d => (
+              <tr>
+                <td>{d.date}</td>
+                <td>{d.time}</td>
+                <td>{d.state}</td>
+                <td>{d.suburb}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
-    )
+    );
   }
 }
 
-export default QRstatComp_table_3
+export default QRstatComp_table_3;
