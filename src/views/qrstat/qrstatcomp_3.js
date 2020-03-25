0459b5caac7bab3b7bd974cf48ada19c767a9582
unknown <pawut.j@gmail.com>
2020-03-25 14:44:59 +0900

    Fix concat name,lastname in identity

--- src/views/qrstat/qrstatcomp_3.js
+++ src/views/qrstat/qrstatcomp_3.js
@@ -1,22 +1,19 @@
-import React,{Component} from "react"
-import {Pie } from "react-chartjs-2";
-import '../../app/app.css';
-import {
-   PieData
-} from "./chartData";
+import React, { Component } from "react";
+import { Pie } from "react-chartjs-2";
+import "../../app/app.css";
+import { PieData } from "./chartData";
 
 class QRstatComp_3 extends Component {
-  render(){
+  render() {
     return (
       <div>
-        <div style = {{color:"#F29258" , textAlign:"center" , marginTop:"3%"}}>
-        <h2>Coming soon</h2>
+        <div style={{ color: "#F29258", textAlign: "center", marginTop: "3%" }}>
+          <h2>Coming soon</h2>
         </div>
         <Pie height={400} data={this.props.data} options={PieData.options} />
-       
       </div>
-    )
+    );
   }
 }
 
-export default QRstatComp_3
+export default QRstatComp_3;
--- src/views/qrstat/qrstatcomp_5.js
+++ src/views/qrstat/qrstatcomp_5.js
@@ -1,43 +1,43 @@
-import React,{Component} from "react"
-import { Table } from 'reactstrap';
-import '../../app/app.css';
+import React, { Component } from "react";
+import { Table } from "reactstrap";
+import "../../app/app.css";
 
 class QRstatComp_5 extends Component {
-
-  render(){
-    console.log(this.props.data)
+  render() {
+    console.log(this.props.data);
     let data = Array.from(this.props.data);
-      return (
-
+    return (
       <div>
-        <div style = {{color:"#F29258" , textAlign:"center" , marginTop:"3%", marginBottom:"3%"}}>
-        <h2>Coming soon</h2>
-        </div>
+        <div
+          style={{
+            color: "#F29258",
+            textAlign: "center",
+            marginTop: "3%",
+            marginBottom: "3%"
+          }}
+        ></div>
         <Table>
-        <thead>
-          <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Phone</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-        {data.map((d) =>
-          <tr>
-            <td>{d.c_fname}</td>
-            <td>{d.c_lname}</td>
-            <td>{d.c_phone_no}</td>
-            <td>{d.c_email}</td>
-          </tr>
-        )
-        }
-        </tbody>
-      </Table>
+          <thead>
+            <tr>
+              <th>Name</th>
 
+              <th>Phone</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(d => (
+              <tr>
+                <td>{`${d.c_fname} ${d.c_lname}`}</td>
+                <td>{d.c_phone_no}</td>
+                <td>{d.c_email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
-    )
+    );
   }
 }
 
-export default QRstatComp_5
+export default QRstatComp_5;
